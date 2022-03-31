06fa79d46b38eaf68cf93ed3ecdad768ae53d419
Luděk Roleček <ludek@rolecek.cz>
2022-03-31 18:52:29 +0200

    Aktualizace seznam

---
+++ src/components/Seznam/index.jsx
@@ -0,0 +1,13 @@
+import React, {useState} from 'react';
+
+const Seznam = () => {
+
+	return (
+		<div className="seznam">
+			<h3>Seznam</h3>
+
+		</div>
+	)
+}
+
+export default Seznam;
--- src/index.jsx
+++ src/index.jsx
@@ -3,11 +3,14 @@ import { render } from 'react-dom';
 import './style.css';
 
 import Komponenta from './components/Komponenta';
+import Seznam from './components/Seznam';
 
 const App = () => {
   return (
     <>
       <h1>Práce v lekci 7</h1>
+      <hr />
+      <Seznam />
       <hr />
       <Komponenta />
     </>
