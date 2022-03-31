93928a50ec94f2de4ed90125a7b4a86b54137cba
Luděk Roleček <ludek@rolecek.cz>
2022-03-31 18:51:11 +0200

    Aktualizace

--- src/components/Komponenta/index.jsx
+++ src/components/Komponenta/index.jsx
@@ -1,10 +1,31 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const Komponenta = () => {
+
+	const [pocet, setPocet] = useState(100);
+
+	const handleClick = () => {
+		setPocet(pocet + 1);
+	}
+
+	const zvetsiPocet = (oKolik) => {
+		// mnoho kodu
+		// slozita logika
+		// blabla bla
+		setPocet(pocet + oKolik);
+	}
+
 
 	return (
 		<div className="Komponenta">
 			<h3>Komponenta</h3>
+			<p>Počet je: {pocet}</p>
+
+			<button onClick={ () => { zvetsiPocet(1) } }>+1</button>
+			<button onClick={ () => { zvetsiPocet(5) } }>+5</button>
+			<button onClick={ () => { zvetsiPocet(10) }}>+10</button>
+
+			<button onClick={handleClick}>Klikni na mě</button>
 		</div>
 	)
 }
