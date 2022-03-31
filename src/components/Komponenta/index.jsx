8bb8747eb74cd0600e4db84ebb08a851adb9e5d6
Petra Bartonova <artep.Bart@seznam.cz>
2022-03-31 19:29:08 +0200

    cvika

--- src/components/Komponenta/index.jsx
+++ src/components/Komponenta/index.jsx
@@ -2,9 +2,13 @@ import React from 'react';
 
 const Komponenta = () => {
 
+	const handleClick = () =>{
+		console.log('klikity klik')
+	}
 	return (
 		<div className="Komponenta">
 			<h3>Komponenta</h3>
+			<button onClick={handleClick}>Klikni na me</button>
 		</div>
 	)
 }
