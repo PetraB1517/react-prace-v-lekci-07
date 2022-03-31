983a1d19fa630ba49cdf7045cff0c90029b9e7ec
Luděk Roleček <ludek@rolecek.cz>
2022-03-31 19:06:40 +0200

    List

--- src/components/Seznam/index.jsx
+++ src/components/Seznam/index.jsx
@@ -7,13 +7,17 @@ const Seznam = () => {
 		'Jana',
 	]);
 
+	const handleClick = () => {
+		setList( [...list, 'Petra'] );
+	}
 
 	return (
 		<div className="seznam">
 			<h3>Seznam</h3>
 			<ul>
-				{ list.map((name, index) => <li key={index}>{name}</li>)}
+				{ list.map((name, index) => <li key={index}>{name}</li>) }
 			</ul>
+			<button onClick={handleClick}>Pridej</button>
 		</div>
 	)
 }
