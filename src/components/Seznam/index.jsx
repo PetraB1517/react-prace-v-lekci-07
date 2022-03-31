8b96eb5e011c2e229d9337cff0cdf86c694facc1
Luděk Roleček <ludek@rolecek.cz>
2022-03-31 18:55:37 +0200

    List

--- src/components/Seznam/index.jsx
+++ src/components/Seznam/index.jsx
@@ -2,10 +2,18 @@ import React, {useState} from 'react';
 
 const Seznam = () => {
 
+	const [list, setList] = useState([
+		'Alena',
+		'Jana',
+	]);
+
+
 	return (
 		<div className="seznam">
 			<h3>Seznam</h3>
-
+			<ul>
+				{ list.map((name, index) => <li key={index}>{name}</li>)}
+			</ul>
 		</div>
 	)
 }
